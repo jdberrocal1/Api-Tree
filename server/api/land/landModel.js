ea015a81b688bf3ccfdb283203a13febe7ed7986
Daniel Berrocal <jdanielb1992@gmail.com>
2016-07-12 17:53:02 -0600

    pre hook problem

--- server/api/land/landModel.js
+++ server/api/land/landModel.js
@@ -4,30 +4,30 @@ var ObjectId = require('mongoose').Types.ObjectId;
 var Schema = mongoose.Schema;
 
 var LandSchema = new Schema({
-  name:{
-    type:String,
-    required:true
-  },
-  province:{
-    type:String,
-    required:true
-  },
-  canton:{
-    type:String,
-    required:true
-  },
-  district:{
-    type:String,
-    required:true
-  },
-  address:{
-    type:String
-  },
-  owner:{
-    type:Schema.Types.ObjectId,
-    ref:'user',
-    required:true
-  }
+    name:{
+        type:String,
+        required:true
+    },
+    province:{
+        type:String,
+        required:true
+    },
+    canton:{
+        type:String,
+        required:true
+    },
+    district:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    }
 });
 
 LandSchema.post('save',function (doc,next){
--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+//var Land = require('../land/landModel');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -31,5 +32,23 @@ var UserSchema = new Schema({
     ]
 
 });
+
+/*UserSchema.pre('remove',function (doc,next){
+    var userObjectId = new ObjectId(this._doc._id.toString());
+    var landsByUser=[];
+
+    Land.find( {"owner": userObjectId}, function (err, lands){
+        if(err){
+            this.invalidate('user', 'There was an error associating the land with an owner');
+        } else if ( lands != null) {
+            var x = lands;
+        } else {
+            this.invalidate('user', 'There is not owner existing');
+        }
+    });
+    next();
+});*/
+
+
 
 module.exports = mongoose.model('user',UserSchema);
